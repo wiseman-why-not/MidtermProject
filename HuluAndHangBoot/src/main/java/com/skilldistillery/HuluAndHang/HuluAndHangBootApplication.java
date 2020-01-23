2d0de1ada686a67863049d54a6083e412dd99fbc
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-01-23 11:34:06 -0700

    Stub Boot/MVC project

---
+++ HuluAndHangBoot/src/main/java/com/skilldistillery/HuluAndHang/HuluAndHangBootApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.HuluAndHang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuluAndHangBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuluAndHangBootApplication.class, args);
+	}
+
+}
---
+++ HuluAndHangBoot/src/test/java/com/skilldistillery/HuluAndHang/HuluAndHangBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.HuluAndHang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuluAndHangBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
