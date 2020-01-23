7ff9172371413f1cd61d5f7c6cc8ee4235bffb61
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2020-01-23 14:16:26 -0700

    info for application.properties

---
+++ HuluAndHangBoot/src/main/java/com/skilldistillery/HuluAndHang/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.HuluAndHang.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping(path = {"/", "home.do"})
+	public String home(){
+		return "index";
+	}
+}
