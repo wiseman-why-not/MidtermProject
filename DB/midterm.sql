ca548b9f841169e5915e91fd15c429f43fa45a3e
sgmerwin <stevegmerwin@gmail.com>
2020-01-25 14:37:11 -0700

    data fix

--- DB/midterm.sql
+++ DB/midterm.sql
@@ -285,7 +285,7 @@ INSERT INTO `user_genre` (`user_id`, `genre_id`) VALUES (3, 7);
 INSERT INTO `user_genre` (`user_id`, `genre_id`) VALUES (4, 8);
 INSERT INTO `user_genre` (`user_id`, `genre_id`) VALUES (4, 9);
 INSERT INTO `user_genre` (`user_id`, `genre_id`) VALUES (5, 10);
-INSERT INTO `user_genre` (`user_id`, `genre_id`) VALUES (5, DEFAULT);
+INSERT INTO `user_genre` (`user_id`, `genre_id`) VALUES (5, 11);
 
 COMMIT;
 
